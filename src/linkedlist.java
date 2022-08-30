f041af64f3870dd4fc9c655476f4960c5d727b86
[Pratheek Gowda] <pratheekgowdadj@gmail.com>
2022-08-30 18:44:41 +0530

    30th august

--- src/linkedlist.java
+++ src/linkedlist.java
@@ -2,6 +2,15 @@ import java.util.LinkedList;
 
 public class linkedlist {
     public static void main(String[] args) {
-        LinkedList<Integer> list = new LinkedList<>();
+        int n = 74, flag = 0;
+        if(n == 0 || n == 1) flag = 1;
+        for (int i = 2; i <= n/2; ++i) {
+                if(n % i == 0){
+                    flag = 1;
+                    break;
+                }
+        }
+        if(flag == 0) System.out.println("The given number is prime");
+        else System.out.println("the given number is not prime");
     }
 }
